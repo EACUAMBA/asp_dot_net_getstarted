b04f671091b2d06df99fa5067c3e8791a9802878
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-08 23:44:29 +0200

    Added Rating property in Product model class.

--- asp_dotnet_getstarted/Models/Product.cs
+++ asp_dotnet_getstarted/Models/Product.cs
@@ -13,5 +13,7 @@ namespace asp_dotnet_getstarted.Models
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
+        public int[] Ratings { get; set; }
     }
 }

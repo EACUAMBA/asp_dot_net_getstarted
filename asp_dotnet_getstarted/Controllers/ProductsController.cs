f44db3f4834ab4e952a3c6cde1010dd9813ceb26
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-08 23:06:35 +0200

    Created my first controller responsible for return a list of products for now.

---
+++ asp_dotnet_getstarted/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using asp_dotnet_getstarted.Models;
+using asp_dotnet_getstarted.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_dotnet_getstarted.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonProductService productService;
+
+        public ProductsController(JsonProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return this.productService.GetProducts();
+        }
+    }
+}

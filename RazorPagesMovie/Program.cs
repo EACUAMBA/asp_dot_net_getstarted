0677eb5463d5c7ffc848bc051771b2e316d93849
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-26 15:27:06 +0200

    Initial commit to build RazorPagesMovie following the tutorial https://learn.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/?view=aspnetcore-7.0

---
+++ RazorPagesMovie/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+//Authorizes a user to access secure resources. This app doesn't use authorization, therefore this line could be removed.
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ RazorPagesMovie/README.md
@@ -0,0 +1,19 @@
+* Learning Razor Pages
+
+*** Used commands
+```bash
+dotnet new webapp -o RazorPagesMovie
+#The dotnet new command creates a new Razor Pages project in the RazorPagesMovie folder.
+```
+
+```bash
+code -r RazorPagesMovie
+#The code command opens the RazorPagesMovie folder in the current instance of Visual Studio Code.
+```
+
+```bash
+dotnet dev-certs https --trust
+#Trusting the HTTPS development certificate
+```
+
+To run in VSCode use **F5** only.

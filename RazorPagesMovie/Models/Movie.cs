e6e93739276b23267c833636053fefd233c520f3
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-08-27 12:00:27 +0200

    Updated the model

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models;
 
@@ -16,7 +17,12 @@ public class Movie
 *Only the date is displayed, not time information.
 **/
     [DataType(DataType.Date)]
+    //The [Display] attribute specifies the display name of a field. In the preceding code, Release Date instead of ReleaseDate.
+    [Display(Name ="Release Date")]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+
+//The [Column(TypeName = "decimal(18, 2)")] data annotation enables Entity Framework Core to correctly map Price to currency in the database. 
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
